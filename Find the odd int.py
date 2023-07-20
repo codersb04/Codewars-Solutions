19f949cb6c2dbd7bc4f95b2c1db6bb0267621eef
codersb04 <subithbabu197@gmail.com>
2023-07-20 22:13:14 +0100

    Create Find the odd int

---
+++ Find the odd int.py
@@ -0,0 +1,19 @@
+"""
+DESCRIPTION:
+Given an array of integers, find the one that appears an odd number of times.
+
+There will always be only one integer that appears an odd number of times.
+
+Examples
+[7] should return 7, because it occurs 1 time (which is odd).
+[0] should return 0, because it occurs 1 time (which is odd).
+[1,1,2] should return 2, because it occurs 1 time (which is odd).
+[0,1,0,1,0] should return 0, because it occurs 3 times (which is odd).
+[1,2,2,3,3,3,4,3,3,3,2,2,1] should return 4, because it appears 1 time (which is odd).
+"""
+def find_it(seq):
+    from collections import Counter
+    a = dict(Counter(seq))
+    for key in a:
+        if a[key] %2 !=0:
+            return key
