570daf6c6c22ee3d9f296bf95c7141c4ecb94d6d
codersb04 <subithbabu197@gmail.com>
2023-08-01 19:54:28 +0100

    Create Find the nth Digit of a Number

---
+++ Find the nth Digit of a Number.py
@@ -0,0 +1,25 @@
+"""
+DESCRIPTION:
+Complete the function that takes two numbers as input, num and nth and return the nth digit of num (counting from right to left).
+
+Note
+If num is negative, ignore its sign and treat it as a positive value
+If nth is not positive, return -1
+Keep in mind that 42 = 00042. This means that findDigit(42, 5) would return 0
+Examples(num, nth --> output)
+5673, 4 --> 5
+129, 2 --> 2
+-2825, 3 --> 8
+-456, 4 --> 0
+0, 20 --> 0
+65, 0 --> -1
+24, -8 --> -1
+"""
+def find_digit(num, nth):
+    num_arr=[i for i in str(num)]
+    if nth>len(num_arr):
+        return 0
+    elif nth<=0:
+        return -1
+    else:
+        return int(num_arr[-nth])
