9e183b1faa77abfb2e9d6e4197db4ad79f4d5afa
codersb04 <subithbabu197@gmail.com>
2023-08-01 19:58:10 +0100

    Create Largest pair sum in array

---
+++ Largest pair sum in array.py
@@ -0,0 +1,12 @@
+"""
+DESCRIPTION:
+Given a sequence of numbers, find the largest pair sum in the sequence.
+
+For example
+
+[10, 14, 2, 23, 19] -->  42 (= 23 + 19)
+[99, 2, 2, 23, 19]  --> 122 (= 99 + 23)
+"""
+def largest_pair_sum(numbers): 
+    numbers.sort()
+    return numbers[-1]+numbers[-2]
